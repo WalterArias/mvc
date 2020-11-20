0c82d0d01a394d32fcc5b3219845d18fa3ab8418
WalterArias <SENA@CARAPRCTIFSD166.FormacionSena.RED>
2020-11-20 10:59:53 -0500

    incluimos el reporteador

---
+++ app/controllers/ListadoCliente.php
@@ -0,0 +1,14 @@
+<?php
+class ListadoCliente extends Controlador
+{
+    public function __construct()
+    {
+        $this->clientemodelo = $this->modelo('ClienteModelo');
+    }
+
+    public function index()
+    {
+        $datos = $this->clientemodelo->obtenerClientes();
+        $this->vista('Reportes/ListadoCliente', $datos);
+    }
+}
