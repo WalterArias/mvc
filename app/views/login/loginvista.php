e15dc2ded5b949455355b177f480132d69c0fad0
WalterArias <SENA@CARAPRCTIFSD166.FormacionSena.RED>
2020-12-11 11:39:09 -0500

    modal con ejemplo

--- app/views/login/loginvista.php
+++ app/views/login/loginvista.php
@@ -16,20 +16,22 @@
     </div>
     <div class="col-md-6">
        
-       
+        
 
 
-
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#frmlogin">Modal</button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#frmlogin">Primary</button>
 
 
         <!-- Button trigger modal -->
   <div class="modal" id="frmlogin" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Ingreso de Usuarios</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
           
-            <div class="modal-body">
-               
+            <div class="modal-body">               
         <form method="POST" action="<?php echo RUTA_URL . 'login/validarIngreso'; ?>">
             <div class="form-group">
                 <label for="login">Usuario</label>
@@ -41,12 +43,17 @@
                 <input type="password" class="form-control" id="password" name="password">
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+            
+    
                 
             </div>
+            <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
 
         </div>
+        </form>
     </div>
 </div>
 
